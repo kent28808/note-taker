497c7a2bb1b8241c56d9e69ede98bb0323a67f8f
kent28808 <kktanoue@gmail.com>
2020-10-14 23:23:14 -0700

    started formatting server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
+var path = require("path"); 
+var fs = require("fs");
+var database
 
 // Sets up the Express App
 // =============================================================
@@ -11,3 +13,30 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+  });
+  
+  // Displays all characters
+  app.get("/api/characters", function(req, res) {
+    return res.json(characters);
+  });
+  
+  // Displays a single character, or returns false
+  app.get("/api/characters/:character", function(req, res) {
+    var chosen = req.params.character;
+  
+    console.log(chosen);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
