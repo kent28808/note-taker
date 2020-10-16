72df1f4e156ed0f3b868871cf120704e83e4dc89
kent28808 <kktanoue@gmail.com>
2020-10-15 18:35:33 -0700

    updated Get and post methods

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var express = require("express");
 var path = require("path"); 
 var fs = require("fs");
-var database
+var data = require("./db/db.json")
+var notes = []
 
 // Sets up the Express App
 // =============================================================
@@ -14,29 +15,29 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Basic route that sends the user first to the AJAX Page
+// Basic routes
+//GET
+//index.html
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+  //notes.html
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+  //api notes
+  app.get("/api/notes", function(req, res) {
+    return res.json(data);
   });
   
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
-  });
-  
-  // Displays all characters
-  app.get("/api/characters", function(req, res) {
-    return res.json(characters);
-  });
-  
-  // Displays a single character, or returns false
-  app.get("/api/characters/:character", function(req, res) {
-    var chosen = req.params.character;
-  
-    console.log(chosen);
+  //POST
+  app.post("/api/notes", function(req, res) {
+    var newNotes = req.body;
+    data.push(newNotes);
+    console.log(newNotes);
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-  
